8f4fc0fe37928d95bd2d0bdba51543c5ea9104cf
ZiDui <94067338+ZiDui@users.noreply.github.com>
2022-10-01 22:42:27 +0800

    Add files via upload

---
+++ js/diy.js
@@ -0,0 +1,11 @@
+$(function(){
+    $('#dowebok').fullpage({
+        'navigation': true,
+        'navigationPosition':"left",
+        'navigationColor':['#fff'],
+
+    });
+    $(window).click(function(){
+		$('audio')[0].play();
+	});
+});
